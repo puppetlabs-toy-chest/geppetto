8a370add2d4b0572ae6e0f184cd3c4a4d6702ccf
Thomas Hallgren <thomas@tada.se>
2014-01-28 08:35:06 +0100

    Use a version 16.0 of Guava

--- com.puppetlabs.geppetto.common/src/com/puppetlabs/geppetto/common/stats/IntegerCluster.java
+++ com.puppetlabs.geppetto.common/src/com/puppetlabs/geppetto/common/stats/IntegerCluster.java
@@ -16,30 +16,29 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
-import com.google.common.collect.Ranges;
 
 /**
  * <p>
  * IntegerCluster utility that implements a simplistic hierarchical clustering algorithm.
  * </p>
  * <p>
- * This class solves the problem; given a series of integers find the smallest number of clusters (of a given min-max inclusive range of values) such
- * that no range is wider than a given max.
+ * This class solves the problem; given a series of integers find the smallest number of clusters (of a given min-max
+ * inclusive range of values) such that no range is wider than a given max.
  * </p>
  * <p>
- * If all values are within the given max, one cluster is produces, and if no two values are closer than max, then there will be as many clusters as
- * there are unique values in the observed set of values.
+ * If all values are within the given max, one cluster is produces, and if no two values are closer than max, then there
+ * will be as many clusters as there are unique values in the observed set of values.
  * </p>
  * <p>
- * The algorithm orders the set of observed values into an set of clusters of 0 size (min = max = value), and searches for the two adjacent clusters
- * that produce the smallest resulting cluster if merged. If the smallest available merge is bigger than the max, the work is done. If the range is
- * smaller than the max, the merged cluster replaces the two inputs. The algorithm now loops back to check for the next two adjacent clusters with the
- * smallest distance.
+ * The algorithm orders the set of observed values into an set of clusters of 0 size (min = max = value), and searches
+ * for the two adjacent clusters that produce the smallest resulting cluster if merged. If the smallest available merge
+ * is bigger than the max, the work is done. If the range is smaller than the max, the merged cluster replaces the two
+ * inputs. The algorithm now loops back to check for the next two adjacent clusters with the smallest distance.
  * </p>
  * <p>
- * The implementation is primarily intended for a fairly small number of observations/clusters as the final step of mapping observations to clusters
- * search (binary search) for a cluster per value. To use this class with larger data sets, it would be better to keep a map from observations to
- * clusters.
+ * The implementation is primarily intended for a fairly small number of observations/clusters as the final step of
+ * mapping observations to clusters search (binary search) for a cluster per value. To use this class with larger data
+ * sets, it would be better to keep a map from observations to clusters.
  * </p>
  */
 public class IntegerCluster {
@@ -73,7 +72,8 @@ public class IntegerCluster {
 	}
 
 	/**
-	 * Compares nodes; the node that starts first is smaller. If starting on the same value, the node that ends first is smaller.
+	 * Compares nodes; the node that starts first is smaller. If starting on the same value, the node that ends first is
+	 * smaller.
 	 */
 	private static class ClusterNodeComparator implements Comparator<ClusterNode> {
 
@@ -205,7 +205,7 @@ public class IntegerCluster {
 		lazyCluster();
 		List<Range<Integer>> result = Lists.newArrayListWithExpectedSize(clusterList.size());
 		for(ClusterNode n : clusterList) {
-			result.add(Ranges.closed(n.min(), n.max()));
+			result.add(Range.closed(n.min(), n.max()));
 		}
 		return result;
 	}
