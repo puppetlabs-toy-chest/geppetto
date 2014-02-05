e4a719126070fffac4bff3d882526abce5a00cdd
Thomas Hallgren <thomas@tada.se>
2014-02-05 08:15:41 +0100

    Allow strings with interpolation of well known constants

--- com.puppetlabs.geppetto.forge/src/main/java/com/puppetlabs/geppetto/forge/util/RubyParserUtils.java
+++ com.puppetlabs.geppetto.forge/src/main/java/com/puppetlabs/geppetto/forge/util/RubyParserUtils.java
@@ -20,10 +20,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.puppetlabs.geppetto.common.os.StreamUtil;
 import org.jrubyparser.CompatVersion;
 import org.jrubyparser.Parser;
 import org.jrubyparser.ast.CallNode;
+import org.jrubyparser.ast.Colon2Node;
+import org.jrubyparser.ast.ConstNode;
+import org.jrubyparser.ast.DStrNode;
+import org.jrubyparser.ast.EvStrNode;
 import org.jrubyparser.ast.NewlineNode;
 import org.jrubyparser.ast.Node;
 import org.jrubyparser.ast.NodeType;
@@ -33,7 +36,52 @@ import org.jrubyparser.ast.SymbolNode;
 import org.jrubyparser.lexer.SyntaxException;
 import org.jrubyparser.parser.ParserConfiguration;
 
+import com.puppetlabs.geppetto.common.os.StreamUtil;
+
 public abstract class RubyParserUtils {
+
+	private static String evalColon2Node(Colon2Node c2cNode) throws IOException {
+		// Check if this is a known constant
+		Node qualNode = c2cNode.getLeftNode();
+		if(qualNode instanceof ConstNode) {
+			String qual = ((ConstNode) qualNode).getName();
+			String name = c2cNode.getName();
+			return getRubyConstant(qual, name);
+		}
+		throw new IOException("Unable to evaluate node " + c2cNode.getName() + "::<expr> into a string");
+	}
+
+	/**
+	 * Perform best-effort evaluation of interpolated string
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private static String evalDStrNode(DStrNode node) throws IOException {
+		StringBuilder bld = new StringBuilder();
+		for(Node child : node.childNodes())
+			bld.append(stringValue(child));
+		return bld.toString();
+	}
+
+	/**
+	 * Perform best-effort evaluation of expression in interpolated string
+	 * 
+	 * @param node
+	 *            The #{} expression
+	 * @return The string that the expression evaluates to
+	 */
+	private static String evalEvStrNode(EvStrNode node) throws IOException {
+		Node body = node.getBody();
+		try {
+			if(body instanceof NewlineNode)
+				// Expressions are aways wrapped in NewlineNode even when there's no newline, go figure
+				return stringValue(((NewlineNode) body).getNextNode());
+		}
+		catch(IOException e) {
+		}
+		return "#{<expr>}";
+	}
 
 	/**
 	 * Find a list of nodes that correspond to a specific path of node types extending
@@ -69,6 +117,27 @@ public abstract class RubyParserUtils {
 			if(child.getNodeType() == searchedType)
 				findNodes(child, path, pathIndex, result);
 		}
+	}
+
+	/**
+	 * Return the value of well known String constants such as File::PATH_SEPARATOR
+	 * 
+	 * @param qual
+	 *            The qualifier, e.g. &quot;File&quot;
+	 * @param name
+	 *            The name, e.g. &quot;PATH_SEPARATOR&quot;
+	 * @return The string value
+	 * @throws IOException
+	 *             when the value is unknown
+	 */
+	private static String getRubyConstant(String qual, String name) throws IOException {
+		if("File".equals(qual)) {
+			if("SEPARATOR".equals(name) || "Separator".equals(name))
+				return File.separator;
+			if("PATH_SEPARATOR".equals(name))
+				return File.pathSeparator;
+		}
+		throw new IOException("Unable to evaluate constant " + qual + "::" + name + " into a string");
 	}
 
 	public static RootNode parse(String id, Reader reader) throws SyntaxException {
@@ -112,6 +181,12 @@ public abstract class RubyParserUtils {
 				return "false";
 			case TRUENODE:
 				return "true";
+			case DSTRNODE:
+				return evalDStrNode((DStrNode) node);
+			case EVSTRNODE:
+				return evalEvStrNode((EvStrNode) node);
+			case COLON2NODE:
+				return evalColon2Node((Colon2Node) node);
 			case CALLNODE: {
 				// We can handle simple string concatenation
 				CallNode argCall = (CallNode) node;
