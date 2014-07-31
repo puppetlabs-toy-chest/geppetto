9b90949ffa2f6724abf4a23fe74b9d2885267ac1
Thomas Hallgren <thomas@tada.se>
2014-07-31 16:46:30 +0200

    Remove all generated artifacts in favor of automated build

--- com.puppetlabs.geppetto.module.dsl.ui/src/com/puppetlabs/geppetto/module/dsl/ui/internal/Activator.java
+++ com.puppetlabs.geppetto.module.dsl.ui/src/com/puppetlabs/geppetto/module/dsl/ui/internal/Activator.java
@@ -6,6 +6,10 @@ import com.google.inject.Injector;
 import com.puppetlabs.geppetto.module.dsl.ui.preferences.ModulePreferencesHelper;
 
 public class Activator extends ModuleActivator {
+	public static String getId() {
+		return getInstance().getBundle().getSymbolicName();
+	}
+
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		Injector injector = getInjector(COM_PUPPETLABS_GEPPETTO_MODULE_DSL_MODULE);
