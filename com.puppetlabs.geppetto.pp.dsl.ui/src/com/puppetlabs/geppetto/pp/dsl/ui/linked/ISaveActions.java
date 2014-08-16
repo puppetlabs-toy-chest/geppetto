881c9f320ea677815c76591d208f0fb15f42883d
Thomas Hallgren <thomas@tada.se>
2014-08-16 10:20:21 +0200

    (GEP-24) Save action with format corrupts file
    The save action may be configured to both modify the document
    by removing trailing spaces and then do a full format.
    
    The save actions were correctly applied to the document inside
    a document modify operation but bad things happened when the
    format operation was called inside that same operation. The
    formatter then attemtped to format the original XtextResource (which
    hadn't been altered) yet using the lenght of the modified document
    with loss of characters at the end as the result.
    
    This commit ensures that the save actions are committed to the
    resource before the formatter kicks in. It also optimizes (per
    suggestion in a TODO comment) so that the formatting is performed
    using the configured IContentFormatterFactory.

--- com.puppetlabs.geppetto.pp.dsl.ui/src/com/puppetlabs/geppetto/pp/dsl/ui/linked/ISaveActions.java
+++ com.puppetlabs.geppetto.pp.dsl.ui/src/com/puppetlabs/geppetto/pp/dsl/ui/linked/ISaveActions.java
@@ -4,19 +4,22 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   Puppet Labs
  */
 package com.puppetlabs.geppetto.pp.dsl.ui.linked;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 
 /**
  * Interface for performing save actions.
- * 
+ *
  */
 public interface ISaveActions {
-	public void perform(IResource r, IXtextDocument document);
+	public void perform(SourceViewerConfiguration sourceViewerConfiguration, ISourceViewer sourceViewer, IResource r,
+			IXtextDocument document);
 }
