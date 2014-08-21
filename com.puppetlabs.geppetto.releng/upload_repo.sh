a1f7d9e5e258142d58c1d8217a1fdc8364425f14
Thomas Hallgren <thomas@tada.se>
2014-08-21 09:29:24 +0200

    Quote paths in aggregation upload script.

--- com.puppetlabs.geppetto.releng/upload_repo.sh
+++ com.puppetlabs.geppetto.releng/upload_repo.sh
@@ -8,9 +8,9 @@
 #
 REPOSITORY_ID=eclipse.xtext
 REPOSITORY_URL=https://geppetto-nexus.puppetlabs.com/content/repositories/eclipse.xtext
-for jarFile in `find $PWD/com $PWD/org -type f -name '*.jar'`; do
+for jarFile in `find "$PWD/com" "$PWD/org" -type f -name '*.jar'`; do
 	pomFile=${jarFile%.jar}.pom
-	mvn deploy:deploy-file -Dfile=$jarFile -DpomFile=$pomFile -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL > /dev/null 2>&1
+	mvn deploy:deploy-file "-Dfile=$jarFile" "-DpomFile=$pomFile" -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL > /dev/null 2>&1
 	exitcode=$?
 	if [ $exitcode -eq 0 ]; then
 		echo 'Deployed ' $jarFile
