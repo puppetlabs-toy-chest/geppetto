57a60bbff5e94d6c8c79c3233c5dea85176472ec
Thomas Hallgren <thomas@tada.se>
2014-09-02 12:02:43 +0200

    Add test of SSL handshake.

--- com.puppetlabs.geppetto.forge.api.tests/src/it/java/com/puppetlabs/geppetto/forge/api/it/ForgeAPITestBase.java
+++ com.puppetlabs.geppetto.forge.api.tests/src/it/java/com/puppetlabs/geppetto/forge/api/it/ForgeAPITestBase.java
@@ -47,5 +47,5 @@ public class ForgeAPITestBase {
 
 	private static final String FORGE_CLIENT_SECRET = "2227c9a7392382f58b5e4d084b705827cb574673ff7d2a5905ef21685fd48e40";
 
-	private static final String FORGE_STAGING_SERVICE_BASE_URL = "https://forgestagingapi.puppetlabs.com";
+	public static final String FORGE_STAGING_SERVICE_BASE_URL = "https://forgestagingapi.puppetlabs.com";
 }
--- com.puppetlabs.geppetto.forge.api.tests/src/it/java/com/puppetlabs/geppetto/forge/v2/api/it/ReleaseTestCreate.java
+++ com.puppetlabs.geppetto.forge.api.tests/src/it/java/com/puppetlabs/geppetto/forge/v2/api/it/ReleaseTestCreate.java
@@ -12,11 +12,28 @@ package com.puppetlabs.geppetto.forge.v2.api.it;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.zip.GZIPOutputStream;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
@@ -59,6 +76,25 @@ public class ReleaseTestCreate extends ForgeAPITestBase {
 		return md;
 	}
 
+	private String[] getCipherSuites() throws NoSuchAlgorithmException, KeyManagementException {
+		// Create an SSLContext that uses our TrustManager
+		SSLContext context = SSLContext.getInstance("TLS");
+		TrustManager tm = new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			}
+
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+		context.init(null, new TrustManager[] { tm }, new SecureRandom());
+		SSLParameters params = context.getSupportedSSLParameters();
+		return params.getCipherSuites();
+	}
+
 	private ModuleName getModuleName() {
 		return ModuleName.create(TEST_USER, testModule, true);
 	}
@@ -92,10 +128,35 @@ public class ReleaseTestCreate extends ForgeAPITestBase {
 	public void testCreate() throws IOException {
 		ModuleName moduleName = getModuleName();
 		byte[] releaseFile = getReleaseImage(moduleName, TEST_RELEASE_VERSION);
-		Release newRelease = ForgeIT.getTestUserForge().createReleaseService().create(
-			moduleName.getOwner(), moduleName.getName(), "Some notes about this release", new ByteArrayInputStream(releaseFile),
-			releaseFile.length);
-		assertNotNull("Null Release", newRelease);
-		assertEquals("Incorrect release version", newRelease.getVersion(), TEST_RELEASE_VERSION);
+		try {
+			Release newRelease = ForgeIT.getTestUserForge().createReleaseService().create(
+				moduleName.getOwner(), moduleName.getName(), "Some notes about this release", new ByteArrayInputStream(releaseFile),
+				releaseFile.length);
+			assertNotNull("Null Release", newRelease);
+			assertEquals("Incorrect release version", newRelease.getVersion(), TEST_RELEASE_VERSION);
+		}
+		catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSSL() throws Exception {
+		URI baseURI = URI.create(FORGE_STAGING_SERVICE_BASE_URL);
+		Socket sock = SSLSocketFactory.getDefault().createSocket();
+		sock.connect(new InetSocketAddress(baseURI.getHost(), 443));
+		try {
+			((SSLSocket) sock).startHandshake();
+		}
+		catch(SSLException e) {
+			StringBuilder bld = new StringBuilder();
+			bld.append("Unable to connect. Supported cipher suites are:\n");
+			for(String suite : getCipherSuites()) {
+				bld.append('\t');
+				bld.append(suite);
+				bld.append('\n');
+			}
+			fail(bld.toString());
+		}
 	}
 }
