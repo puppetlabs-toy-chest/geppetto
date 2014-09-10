97905811f9fd81aca655037aafcb7671d8aad89a
Thomas Hallgren <thomas@tada.se>
2014-09-10 14:48:35 +0200

    Replace inadvertently added file:// URL in install script.

--- com.puppetlabs.geppetto.releng/buckminster-install.sh
+++ com.puppetlabs.geppetto.releng/buckminster-install.sh
@@ -6,7 +6,7 @@ INSTALLATION_DIR=`pwd`
 
 $DIRECTOR_HOME/director\
  -p2.os linux -p2.ws gtk -p2.arch x86_64\
- -r file:/home/thhal/build.output/org.eclipse.buckminster.site.eclipse.headless_1.6.0-eclipse.feature/site.p2\
+ -r http://download.eclipse.org/tools/buckminster/headless-4.4\
  -r http://download.eclipse.org/releases/luna\
  -i org.eclipse.buckminster.cmdline.product\
  -i org.eclipse.buckminster.core.headless.feature.feature.group\
@@ -15,8 +15,8 @@ $DIRECTOR_HOME/director\
  -i org.eclipse.buckminster.git.headless.feature.feature.group\
  -i org.eclipse.equinox.p2.repository.tools\
  -i org.eclipse.emf.ant\
- -i org.eclipse.emf.mwe2.launch\
  -i org.eclipse.emf.importer.ecore\
+ -i org.eclipse.emf.mwe2.launch\
  -d $INSTALLATION_DIR/buckminster\
  -p Buckminster
 
