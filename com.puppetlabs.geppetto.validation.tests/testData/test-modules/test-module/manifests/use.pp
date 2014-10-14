3a9f2f854a0ff441c2e84205d8823672097d2521
Thomas Hallgren <thomas@tada.se>
2014-10-14 17:23:54 +0200

    (GEP-192) Fix failing test
    A test relied on that something found by the scanner was reported
    as "Not found on search path" although the declaration was found
    in the same file as the reference. This commit adds a file that
    makes a reference from another file, thus testing both cases
    (found in search path and not found).

---
+++ com.puppetlabs.geppetto.validation.tests/testData/test-modules/test-module/manifests/use.pp
@@ -0,0 +1,14 @@
+# Class: test-module
+#
+# This module ensures java of at least version 1.5 is installed
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+include test_module
