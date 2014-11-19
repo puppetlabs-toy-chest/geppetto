96d24b8b8d7afb6bb39fd654bec7f6ce3bc5fea4
Thomas Hallgren <thomas@tada.se>
2014-11-19 12:55:01 +0100

    Add name, version, and release fields to docs.json

--- com.puppetlabs.geppetto.forge.api/src/main/java/com/puppetlabs/geppetto/forge/model/ForgeDocs.java
+++ com.puppetlabs.geppetto.forge.api/src/main/java/com/puppetlabs/geppetto/forge/model/ForgeDocs.java
@@ -18,19 +18,71 @@ public class ForgeDocs {
 
 	private Collection<NamedDocItem> functions;
 
+	private String name;
+
+	private String version;
+
+	private VersionedName release;
+
 	public Collection<NamedDocItem> getFunctions() {
 		return functions;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the release
+	 */
+	public VersionedName getRelease() {
+		return release;
 	}
 
 	public Collection<Type> getTypes() {
 		return types;
 	}
 
+	/**
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
 	public void setFunctions(Collection<NamedDocItem> functions) {
 		this.functions = functions;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param release
+	 *            the release to set
+	 */
+	public void setRelease(VersionedName release) {
+		this.release = release;
 	}
 
 	public void setTypes(Collection<Type> types) {
 		this.types = types;
 	}
+
+	/**
+	 * @param version
+	 *            the version to set
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
 }
