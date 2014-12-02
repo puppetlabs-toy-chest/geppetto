beeb88e720e0176a0410591da7f8973ec3869494
Thomas Hallgren <thomas@tada.se>
2014-12-02 13:34:07 +0100

    (GEP-213) Simplified route planner

--- com.puppetlabs.geppetto.injectable.eclipse/src/com/puppetlabs/geppetto/injectable/eclipse/impl/ProxiedRoutePlanner.java
+++ com.puppetlabs.geppetto.injectable.eclipse/src/com/puppetlabs/geppetto/injectable/eclipse/impl/ProxiedRoutePlanner.java
@@ -10,16 +10,13 @@
  */
 package com.puppetlabs.geppetto.injectable.eclipse.impl;
 
-import java.net.InetAddress;
 import java.net.URI;
 
 import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
-import org.apache.http.conn.params.ConnRouteParams;
 import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.conn.routing.HttpRoutePlanner;
-import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.protocol.HttpContext;
 import org.eclipse.core.net.proxy.IProxyData;
@@ -35,50 +32,19 @@ public class ProxiedRoutePlanner implements HttpRoutePlanner {
 
 	@Override
 	public HttpRoute determineRoute(HttpHost target, HttpRequest request, HttpContext context) throws HttpException {
-		if(request == null) {
-			throw new IllegalStateException("Request must not be null.");
-		}
+		if(request == null || target == null)
+			throw new IllegalStateException("target and request must not be null.");
 
-		// If we have a forced route, we can do without a target.
-		HttpRoute route = ConnRouteParams.getForcedRoute(request.getParams());
-		if(route != null)
-			return route;
-
-		// If we get here, there is no forced route.
-		// So we need a target to compute a route.
-
-		if(target == null) {
-			throw new IllegalStateException("Target host must not be null.");
-		}
-
-		final InetAddress local = ConnRouteParams.getLocalAddress(request.getParams());
-		final HttpHost proxy = ConnRouteParams.getDefaultProxy(request.getParams());
-
-		final Scheme schm;
-		try {
-			schm = schemeRegistry.getScheme(target.getSchemeName());
-		}
-		catch(IllegalStateException ex) {
-			throw new HttpException(ex.getMessage());
-		}
-		// as it is typically used for TLS/SSL, we assume that
-		// a layered scheme implies a secure connection
-		final boolean secure = schm.isLayered();
-
-		if(proxy != null)
-			return new HttpRoute(target, local, proxy, secure);
-
+		boolean secure = schemeRegistry.getScheme(target.getSchemeName()).isLayered();
 		IProxyData[] select = Activator.getInstance().getProxyService().select(URI.create(target.toURI()));
-		for(IProxyData proxyData : select)
-			if(proxyData.getType().equals(IProxyData.HTTP_PROXY_TYPE)) {
-				HttpHost proxyHost = new HttpHost(proxyData.getHost(), proxyData.getPort());
-				return new HttpRoute(target, null, proxyHost, secure);
-			}
-			else if(proxyData.getType().equals(IProxyData.HTTPS_PROXY_TYPE)) {
-				HttpHost proxyHost = new HttpHost(proxyData.getHost(), proxyData.getPort(), "https");
-				return new HttpRoute(target, null, proxyHost, secure);
-			}
+		for(IProxyData pd : select)
+			if(pd.getType().equals(IProxyData.HTTPS_PROXY_TYPE))
+				return new HttpRoute(target, null, new HttpHost(pd.getHost(), pd.getPort(), "https"), secure);
 
-		return new HttpRoute(target, local, secure);
+		for(IProxyData pd : select)
+			if(pd.getType().equals(IProxyData.HTTP_PROXY_TYPE))
+				return new HttpRoute(target, null, new HttpHost(pd.getHost(), pd.getPort()), secure);
+
+		return new HttpRoute(target, null, secure);
 	}
 }
